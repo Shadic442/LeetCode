1468a2949bde9bb6d6fdedac795981dafb5c9d0f
Shadic442 <56701897+Shadic442@users.noreply.github.com>
2025-01-12 21:03:58 -0500

    tested initializing of variables for listnodes

--- 21-Merge_Two_Sorted_Lists/src/test/MergeTwoSortedLists.java
+++ 21-Merge_Two_Sorted_Lists/src/test/MergeTwoSortedLists.java
@@ -6,6 +6,18 @@ public class MergeTwoSortedLists {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ListNode list1 = new ListNode(1);
+		list1.next = new ListNode(2);
+		list1.next.next = new ListNode(4);
+		System.out.println("List 1 : ");
+		printList(list1);
+		
+		ListNode list2 = new ListNode(1);
+		list2.next = new ListNode(3);
+		list2.next.next = new ListNode(4);
+		System.out.println("List 2 : ");
+		printList(list2);
 	}
 
 	/**
@@ -32,5 +44,16 @@ public class MergeTwoSortedLists {
 		ArrayList<Integer> sortedList = new ArrayList<Integer>();
 		return list2;
 
+	}
+	
+	static void printList(ListNode current) {
+		while (current != null) {
+			System.out.println(current.val);
+			if (current.next != null) {
+				System.out.println(" ");
+			}
+			current = current.next;
+		}
+		System.out.println();
 	}
 }
